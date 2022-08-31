50514c6437a2dea38d69766eb3ebcb57b5d2515f
Patricio Espinoza <p.alfonsoespinoza@gmail.com>
2022-08-31 12:40:33 -0400

    todolist and fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,115 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useEffect, useRef, useState } from "react";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+function Todolist() {
+  let nombreRef = useRef(null);
+  const [task, setTask] = useState ([]);
 
-export default Home;
+  const [urlApi] = useState("http://assets.breatheco.de/apis/fake/todos/user/SussanHernandez")//declara api
+
+  useEffect(() => {
+    getTask(urlApi)
+  }, [])
+ 
+  const getTask = url => {
+    fetch(url)
+      .then(Response => Response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+  }
+  
+  const getUser = url => {
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify([]),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(Response => Response.json())
+      .then(data => console.log(data.result))
+      .catch(error => console.log(error));
+  };
+  
+  const updateTask = (url, task) => {
+    fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(task),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+  }
+
+  const addTask = (e) => {
+    if (e.keyCode === 13 && nombreRef.value !== "") {
+      setTask(task.concat(nombreRef.value));
+      nombreRef.value = "";
+    }
+  };
+
+  const deleteTask = (index) =>{
+    task.splice(index,1);
+    setTask([...task]);
+  };
+
+  const deleteAll= () => {
+
+    fetch(urlApi, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(response => response.json()
+    ).then(data => console.log(data)
+
+    ).catch(error => console.log(error))
+
+  }
+
+
+  return (
+    <div className="container">
+      <div className="card mt-4">
+        <div className="card-body">
+          <h1 className="card-title text-center">To do list <i className="fas fa-tasks"></i></h1> 
+          <ul className="list-group list-group-flush">
+            <div className="input-group mb-2 list-group list-group-flush">
+              <input
+                onKeyUp={addTask}
+                ref={(r) => (nombreRef = r)}
+                type="text"
+                id="input"
+                className="list-group-item"
+                placeholder="What needs to be done?"
+              />
+            </div>
+
+            {!!task.length > 0 &&
+              task.map((valor, index) => {
+                return (
+                  <li class="list-group-item" key={index}>
+                    {valor}{" "}
+                    <i
+                      className="fas fa-trash float-right"
+                      id="eliminar"
+                      onClick={() => deleteTask(index)}
+                    ></i>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+        <div className="card-footer text-muted">
+          Things to do {task.length}
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Todolist;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,21 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import Todolist from "./component/home.jsx";
 
 //import your own components
 import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+  reportWebVitals();
